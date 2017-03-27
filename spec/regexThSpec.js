f7d11d62739a9b09cba5a409d92be92942148fec
vorabhol chuaybhan <max003003003@gmail.com>
2017-03-28 00:08:08 +0700

    implement new spect

--- spec/regexThSpec.js
+++ spec/regexThSpec.js
@@ -11,6 +11,13 @@ describe('Regex wednesday test',function(){
 describe('xx โมง xx นาที test',function(){
   it("should be able to make correct time", function(){
       var result =  splitWordWithPlusSign("วัน พุธ 9 โมง 5 นาที ") 
-    expect("09:05").toEqual(result.output[0].format("HH:mm"))
+      expect("09:05").toEqual(result.output[0].format("HH:mm"))
+
+      result2 = splitWordWithPlusSign("วันอาทิตย์ ไปกินข้าวตอน 10.45 11 โมง 45 นาที โรบินสัน วันจันทร์ 8 โมง เรียนสัมนา ม.เกษตร ศรีราชา ไปเที่ยวทะเลพัทยา,ไหว้พระ พุทธมน 18:00")
+      expect("2017-04-02 11:45").toEqual(result2[0].output[0].format("YYYY-MM-DD HH:mm"))
+      expect("2017-04-02 10:45").toEqual(result2[0].output[1].format("YYYY-MM-DD HH:mm"))
+      expect("2017-04-03 08:00").toEqual(result2[1].output[0].format("YYYY-MM-DD HH:mm"))
+      expect("2017-04-03 18:00").toEqual(result2[1].output[1].format("YYYY-MM-DD HH:mm"))
+
   })
 })
